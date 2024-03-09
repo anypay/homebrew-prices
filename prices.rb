76e2274e93601a6464b17138ba221e076d100e91
My CTO <mycto@proton.me>
2024-03-09 13:02:08 +0100

    Update to version 0.0.17

--- prices.rb
+++ prices.rb
@@ -1,16 +1,16 @@
-BUILD_VERSION = "0.0.15"
-AMD_SHASUM_256 = "3a1feb84b7bf5f1c89d5da08a15135cd7416e59c0c5c252b48ee18d03f37db0c"
-ARM_SHASUM_256 = "c8b47302775dac77312e64e9c9c6698f0f58aa51243ff6d8e5ebacee2bef6f5a"
+BUILD_VERSION = "0.0.17"
+AMD_SHASUM_256 = "6737b5bf34ff8cae8daf1f58d8c8c1253702c274c2b73e6c564411ae1867c5c7"
+ARM_SHASUM_256 = "f5d09e3d4c38e52383c4e54b129cd41bd38ef4ee5b4bb53d698256a8697d4959"
 
 class Prices < Formula
   desc "Prices Server"
   homepage "https://github.com/anypay/prices-rs"
-  url "https://github.com/anypay/prices-rs/releases/download/0.0.15/prices-0.0.15-macos-64-bit.zip"
+  url "https://github.com/anypay/prices-rs/releases/download/0.0.17/prices-0.0.17-macos-64-bit.zip"
   sha256 AMD_SHASUM_256
 
   # For ARM architecture
   on_arm do
-    url "https://github.com/anypay/prices-rs/releases/download/0.0.15/prices-0.0.15-macos-arm.zip"
+    url "https://github.com/anypay/prices-rs/releases/download/0.0.17/prices-0.0.17-macos-arm.zip"
     sha256 ARM_SHASUM_256
   end
 
